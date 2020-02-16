a39f8265e61e5d9e2055501bfdbdbc02b9831ac0
Lorena <gyeol0710@naver.com>
2020-02-16 15:27:35 +0900

    good

---
+++ Assets/Scripts/RobotMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotMove : MonoBehaviour
+{
+    Transform tr;
+    float speed;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        tr.Translate(Vector2.left * speed);
+    }
+}
