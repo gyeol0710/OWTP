ab2ae8a9c239ca8b6a282122e0deb8e8ee6516da
Lorena <gyeol0710@naver.com>
2020-02-21 01:48:45 +0900

    로봇이 움직인다
    로봇이 움직이는 것에 만족하자

---
+++ Assets/Scripts/RobotManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotManager : MonoBehaviour
+{
+    public Sprite RobotL;
+    public Sprite RobotR;
+    private SpriteRenderer RobotRenderer;
+
+    Transform tr;
+    public float Speed;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        RobotRenderer = gameObject.GetComponent<SpriteRenderer>();
+        RobotRenderer.sprite = RobotL;
+    }
+
+    void Update()
+    {
+        tr.Translate(Vector3.left * Speed);
+    }
+}
