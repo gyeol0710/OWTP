4728fcbab3d8c147d9231a85147484f4c47c9e2f
Lorena <gyeol0710@naver.com>
2020-08-01 13:55:39 +0900

    Beta v1.3.0a
    - 사운드 출력 개선

--- Assets/Scripts/DevTestingScr.cs
+++ Assets/Scripts/DevTestingScr.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-using Unity.UNetWeaver;
 
 public class DevTestingScr : MonoBehaviour
 {
